454f96573661275b2c69c0b0fbd67a6656cf5d88
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-08-05 22:43:22 -0400

    If Statement

--- ifstatement.php
+++ ifstatement.php
@@ -7,7 +7,32 @@
     <title>Simple If Statement</title>
 </head>
 <body>
-    <h1>Simple If Statement</h1>
-    
+    <?php echo '<h1>Simple If Statement</h1>';
+     
+        // An if statement that will carry out an action based on the value of the variable.
+        
+        $grade = 80;
+
+        if($grade >= 50){
+            echo '<h3 style = "color : green"> You have passed</h3>';
+        }else{
+            echo '<h3 style = "color : red"> You have failed</h3>';
+        }
+
+        $grade = 'B';
+
+        if ($grade == 'A') {
+            echo '<h3 style = "color : green"> You are a superstar</h3>';
+        }
+        elseif ($grade == 'B') {
+            echo '<h3 style = "color : blue"> You did well</h3>';
+        }
+        else {
+            echo '<h3 style = "color : red"> You have failed</h3>';
+        }
+
+
+        
+    ?>
 </body>
 </html>
