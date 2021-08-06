f15f71bb916324204fae4794d150ac1b5724e77e
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-08-06 06:00:17 -0400

    PhP Array and manipulation

--- array.php
+++ array.php
@@ -8,6 +8,24 @@
 </head>
 <body>
     <h1>Simple Array and  Printouts</h1>
+
+    <?php 
+
+        // a variable
+        $num = 3;
+
+        //an array
+        $numbers = array(1,2,3,4,5,6,7,8,9,10);
+
+        $size = count($numbers);
+        
+        echo '<p> Array numbers is equal to '. $size .' .';
+
+        for($count = 0; $count < $size; $count++){
+            echo "<p>$numbers[$count]</p>";
+        }
+    
+    ?>
     
 </body>
 </html>
